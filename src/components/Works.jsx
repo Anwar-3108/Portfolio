e3429975295105bc8778227ec38e459557cc29d8
Anwar-3108 <syed.sahab9628@gmail.com>
2023-10-07 13:10:34 +0530

    .

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -27,11 +27,13 @@ const ProjectCard = ({
         className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
+        <a href="https://magma-01.netlify.app/">
           <img
             src={image}
             alt='project_image'
             className='w-full h-full object-cover rounded-2xl'
           />
+          </a>
 
           <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
             <div
